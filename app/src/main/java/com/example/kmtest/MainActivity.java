13d53f9851611270f38c534c2d2af1d96ce90543
jnuwxc <1165124074@qq.com>
2021-04-29 16:58:37 +0800

    设置界面的整体结构，标题栏，前三行。

--- app/src/main/java/com/example/kmtest/MainActivity.java
+++ app/src/main/java/com/example/kmtest/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.kmtest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(it->{
+            Intent intent = new Intent(this, SettingActivity.class);
+            startActivity(intent);
+        });
+
+
     }
 }
