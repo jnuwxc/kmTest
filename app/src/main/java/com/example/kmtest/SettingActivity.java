144bf348140d1ace57d8bf10275f226b3197c232
jnuwxc <1165124074@qq.com>
2021-05-08 17:14:56 +0800

    设置界面控制栏字体改为黑色

--- app/src/main/java/com/example/kmtest/SettingActivity.java
+++ app/src/main/java/com/example/kmtest/SettingActivity.java
@@ -2,8 +2,11 @@ package com.example.kmtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.ViewCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowInsetsController;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -12,5 +15,16 @@ public class SettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         getSupportActionBar().hide();
+
+        // 将状态栏字体颜色设置为黑色
+        // 关于WindowInsetsController，查看下面的链接
+        // https://developer.android.com/reference/android/view/WindowInsetsController?hl=en
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            WindowInsetsController insetsController = getWindow().getInsetsController();
+            insetsController.setSystemBarsAppearance(WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS);
+        }else{
+            // API30以下
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
     }
 }
